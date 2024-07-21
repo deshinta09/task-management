ce3a4c191d3602d9c3b30ff2f3a7c23bd56a3ff5
deshinta <deshintaaulia065@gmail.com>
2024-07-21 19:59:18 +0700

    feat: add api_auth

---
+++ api_auth/config/auth.php
@@ -0,0 +1,28 @@
+<?php
+/** @file config/auth.php */
+
+return [
+    'defaults' => [
+        'guard' => 'api',
+        'passwords' => 'users',
+    ],
+    'guards' => [
+        'api' => [
+            'driver' => 'passport',
+            'provider' => 'users',
+        ],
+    ],
+    'providers' => [
+        'users' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\User::class,
+        ],
+    ],
+    'passwords' => [
+        'users' => [
+            'provider' => 'users',
+            'table' => 'password_resets',
+            'expire' => 60,
+        ],
+    ],
+];
